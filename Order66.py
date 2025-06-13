8a9ac6333009ac72c759f9e2e33816692f4ac4f5
GianG43 <giangrob@gmx.ch>
2025-06-13 11:53:36 +0200

    Test 2

---
+++ Order66.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+def say_hi(name=""):
+    print("Hello", name)
+
+while True:
+    name = input("Your name: ")
+    if name in ("Stop", "stop", "Exit", "exit"):
+        break
+    say_hi(name)
+
+
+# In[ ]:
+
+
+
+
